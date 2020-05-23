6ba58dc6379cd07be09a77f839a6a4bf4e469ca2
DonOkazkar <43777738+DonOkazkar@users.noreply.github.com>
2020-05-23 17:08:28 +0100

    Add files via upload

---
+++ database.php
@@ -0,0 +1,12 @@
+<?php
+$baseurl = "http://localhost/jw7/admin/";	
+$dbname = "smartjournal";
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+
+$chattitle = 'Messages';
+$chatsite = 'http://localhost/jw7/chat/';
+$chatuser = 'http://localhost/jw7/chat/';
+
+?>
